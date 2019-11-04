98e4254eddc0169d44742ae1c4e717b5a12580df
Anirban Chetia <bloodraven166@gmail.com>
2019-11-04 20:11:10 +0530

    Disjoint

---
+++ Disjoint.cpp
@@ -0,0 +1,23 @@
+void initialize( int Arr[ ], int N)
+    {
+        for(int i = 0;i<N;i++)
+        Arr[ i ] = i ;
+    }
+    //returns true if A and B are connected, else returns false 
+     bool find( int Arr[ ], int A, int B)                           
+    {
+    if(Arr[ A ] == Arr[ B ])
+    return true;
+    else
+    return false;   
+    }
+    //change all entries from arr[ A ] to arr[ B ].
+    void union(int Arr[ ], int N, int A, int B)
+    {
+        int TEMP = Arr[ A ];
+    for(int i = 0; i < N;i++)
+        {
+        if(Arr[ i ] == TEMP)
+        Arr[ i ] = Arr[ B ]; 
+        }
+    }
