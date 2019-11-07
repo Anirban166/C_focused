d06c2f65e4ab3ec4a917ff8456372032d601fefb
Anirban Chetia <bloodraven166@gmail.com>
2019-11-07 21:09:10 +0530

    Remove Kth node from head of SLL

---
+++ LL/RemoveKthEnd.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+using namespace std;
+
+class LinkedList 
+{ public: int value; LinkedList *next;
+  LinkedList(int value);
+  void addMany(vector<int> values); 
+  vector<int>getNodesInArray();
+};
+
+void removeKthNodeFromEnd(LinkedList *head, int k)
+{ int counter=1;
+  LinkedList *first=head,*second=head;
+  while(counter<=k) 
+  { second=second->next;
+    counter++;
+  }
+  if(second==NULL)
+  { head->value=head->next->value;
+    head->next=head->next->next;
+    return;
+  }
+  while(second->next!=NULL)
+  { second=second->next;
+    first=first->next;
+  }
+  first->next=first->next->next;
+}
+  
+  
